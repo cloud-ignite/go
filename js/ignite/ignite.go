c3388d5232de42aadd90a0366f7d405e0fe52f68
Daniel Renne <Daniel.Renne@atlona.com>
2016-08-08 11:55:47 -0400

    Fixed ignite issues with Arrays and added more CSS binding.

--- js/ignite/ignite.go
+++ js/ignite/ignite.go
@@ -2,7 +2,7 @@
 package ignite
 
 import (
-	"fmt"
+	// "fmt"
 	"honnef.co/go/js/dom"
 	"reflect"
 	"strconv"
@@ -10,7 +10,11 @@ import (
 	"sync"
 )
 
-const VIRTUAL_DOM_INNERHTML = "data-bind-innerhtml"
+const (
+	VIRTUAL_DOM_INNERHTML = "data-bind-innerhtml"
+	VIRTUAL_DOM_TOP       = "data-bind-top"
+	VIRTUAL_DOM_LEFT      = "data-bind-left"
+)
 
 type ObservableHandler func(string)
 
@@ -65,7 +69,7 @@ func (self *VirtualDom) BindDocument(document dom.Document) error {
 			var add bool
 
 			switch key {
-			case VIRTUAL_DOM_INNERHTML:
+			case VIRTUAL_DOM_INNERHTML, VIRTUAL_DOM_LEFT, VIRTUAL_DOM_TOP:
 				add = true
 				break
 			}
@@ -112,7 +116,7 @@ func (self *VirtualDom) handleObservableCallback(key string) {
 
 func (self *VirtualDom) bindDOM(key string) {
 
-	fmt.Println("BindingDom")
+	// fmt.Println("BindingDom")
 	//First Get the Object observed to change
 	obj := self.ObservableObjects[key]
 
@@ -127,15 +131,21 @@ func (self *VirtualDom) bindDOM(key string) {
 		if ok {
 			for key, value := range attribute.Keys {
 
-				// fmt.Println("printing the Property")
-				// fmt.Printf("%v\n", value)
-
 				//Use Reflection to get the value of the obj
 				propertyValue := getReflectionValue(value, obj)
+				htmlElement := binding.Element.(dom.HTMLElement)
 
 				switch key {
 				case VIRTUAL_DOM_INNERHTML:
 					binding.Element.SetInnerHTML(propertyValue)
+					break
+				case VIRTUAL_DOM_LEFT:
+					cssUnit := getCSSUnit(htmlElement.Style().GetPropertyValue("left"))
+					htmlElement.Style().SetProperty("left", propertyValue+cssUnit, "important")
+					break
+				case VIRTUAL_DOM_TOP:
+					cssUnit := getCSSUnit(htmlElement.Style().GetPropertyValue("top"))
+					htmlElement.Style().SetProperty("top", propertyValue+cssUnit, "important")
 					break
 				}
 			}
@@ -146,7 +156,14 @@ func (self *VirtualDom) bindDOM(key string) {
 func getReflectionValue(key string, x interface{}) string {
 
 	splitKey := strings.Split(key, ".")
+
 	propertyName := splitKey[0]
+	originalPropertyName := propertyName
+
+	arrayIndex := strings.Index(propertyName, "[")
+	if arrayIndex != -1 {
+		propertyName = propertyName[:arrayIndex]
+	}
 
 	val := reflect.ValueOf(x).Elem()
 
@@ -177,11 +194,32 @@ func getReflectionValue(key string, x interface{}) string {
 			return strconv.FormatFloat(val.Float(), 'E', -1, 64)
 		case reflect.String:
 			return val.String()
+		case reflect.Array, reflect.Slice:
+
+			lastIndex := strings.Index(originalPropertyName, "]")
+			index := StringToInt(originalPropertyName[arrayIndex+1 : lastIndex])
+			arrayItem := val.Index(index)
+
+			switch arrayItem.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				return strconv.FormatInt(arrayItem.Int(), 10)
+			case reflect.Float32:
+				return strconv.FormatFloat(arrayItem.Float(), 'E', -1, 32)
+			case reflect.Float64:
+				return strconv.FormatFloat(arrayItem.Float(), 'E', -1, 64)
+			case reflect.String:
+				return arrayItem.String()
+
+			case reflect.Struct:
+				if len(splitKey) > 1 {
+					return getStructReflectionValue(strings.Replace(key, originalPropertyName+".", "", 1), arrayItem)
+				}
+			}
+
 		case reflect.Struct:
 			if len(splitKey) > 1 {
 				return getStructReflectionValue(strings.Replace(key, propertyName+".", "", 1), val)
 			}
-
 		}
 	}
 	return ""
@@ -190,7 +228,14 @@ func getReflectionValue(key string, x interface{}) string {
 func getStructReflectionValue(key string, val reflect.Value) string {
 
 	splitKey := strings.Split(key, ".")
+
 	propertyName := splitKey[0]
+	originalPropertyName := propertyName
+
+	arrayIndex := strings.Index(propertyName, "[")
+	if arrayIndex != -1 {
+		propertyName = propertyName[:arrayIndex]
+	}
 
 	for i := 0; i < val.NumField(); i++ {
 
@@ -219,6 +264,27 @@ func getStructReflectionValue(key string, val reflect.Value) string {
 			return strconv.FormatFloat(val.Float(), 'E', -1, 64)
 		case reflect.String:
 			return val.String()
+		case reflect.Array, reflect.Slice:
+
+			lastIndex := strings.Index(originalPropertyName, "]")
+			index := StringToInt(originalPropertyName[arrayIndex+1 : lastIndex])
+			arrayItem := val.Index(index)
+
+			switch arrayItem.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				return strconv.FormatInt(arrayItem.Int(), 10)
+			case reflect.Float32:
+				return strconv.FormatFloat(arrayItem.Float(), 'E', -1, 32)
+			case reflect.Float64:
+				return strconv.FormatFloat(arrayItem.Float(), 'E', -1, 64)
+			case reflect.String:
+				return arrayItem.String()
+
+			case reflect.Struct:
+				if len(splitKey) > 1 {
+					return getStructReflectionValue(strings.Replace(key, originalPropertyName+".", "", 1), arrayItem)
+				}
+			}
 		case reflect.Struct:
 			if len(splitKey) > 1 {
 				return getStructReflectionValue(strings.Replace(key, propertyName+".", "", 1), val)
@@ -227,4 +293,53 @@ func getStructReflectionValue(key string, val reflect.Value) string {
 		}
 	}
 	return ""
+}
+
+func getCSSUnit(s string) string {
+	if strings.Contains(s, "px") {
+		return "px"
+	} else if strings.Contains(s, "em") {
+		return "em"
+	} else if strings.Contains(s, "ex") {
+		return "ex"
+	} else if strings.Contains(s, "ch") {
+		return "ch"
+	} else if strings.Contains(s, "rem") {
+		return "rem"
+	} else if strings.Contains(s, "vw") {
+		return "vw"
+	} else if strings.Contains(s, "vh") {
+		return "vh"
+	} else if strings.Contains(s, "vmin") {
+		return "vmin"
+	} else if strings.Contains(s, "vmax") {
+		return "vmax"
+	} else if strings.Contains(s, "%") {
+		return "%"
+	} else if strings.Contains(s, "cm") {
+		return "cm"
+	} else if strings.Contains(s, "mm") {
+		return "mm"
+	} else if strings.Contains(s, "in") {
+		return "in"
+	} else if strings.Contains(s, "pt") {
+		return "pt"
+	} else if strings.Contains(s, "pc") {
+		return "pc"
+	}
+
+	return "px"
+}
+
+func StringToInt(val string) int {
+
+	r, err := strconv.Atoi(val)
+	if err != nil {
+		return 0
+	}
+	return r
+}
+
+func IntToString(val int) string {
+	return strconv.Itoa(val)
 }
